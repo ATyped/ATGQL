d972d111dea224832383ad0c654893bcbe7da1de
iyanging <iyanging@163.com>
2021-07-09 00:51:49 +0800

    feat(pyutils): transform devAssert.ts -> dev_assert.py

---
+++ atgql/pyutils/dev_assert.py
@@ -0,0 +1,8 @@
+from typing import Any, Final
+
+
+def dev_assert(condition: Any, message: str) -> None:
+    boolean_condition: Final[bool] = bool(condition)
+
+    if not boolean_condition:
+        raise Exception(message)
