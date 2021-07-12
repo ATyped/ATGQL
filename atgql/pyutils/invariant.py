9f23255d469a95e5e5952d544cd1eb00b2a2de4f
iyanging <iyanging@163.com>
2021-07-13 00:33:07 +0800

    feat(pyutils): transform invariant.ts -> invariant.py

---
+++ atgql/pyutils/invariant.py
@@ -0,0 +1,7 @@
+from typing import Any, Optional
+
+
+def invariant(condition: Any, message: Optional[str] = None):
+    boolean_condition = bool(condition)
+    if not boolean_condition:
+        raise Exception(message if message is not None else 'Unexpected invariant triggered.')
