3985fdf4878ee4db68562c1c7f22dde32d46aed8
iyanging <iyanging@163.com>
2021-07-16 00:55:47 +0800

    docs(pyutils.key_map): add docstring for key_map()

--- atgql/pyutils/key_map.py
+++ atgql/pyutils/key_map.py
@@ -7,6 +7,11 @@ T = TypeVar('T')
 
 
 def key_map(array: Sequence[T], key_fn: Callable[[T], str]) -> ObjMap[T]:
+    """
+    Creates a keyed dict from an array, given a function to produce the keys
+    for each value in the array.
+    """
+
     result: Final[dict] = {}
     for item in array:
         result[key_fn(item)] = item
