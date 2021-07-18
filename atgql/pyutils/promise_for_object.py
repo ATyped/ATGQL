570bac89d429aa3d053f179abc181aa63d943f9d
iyanging <iyanging@163.com>
2021-07-18 12:39:10 +0800

    feat(pyutils): implement promise_for_object()

---
+++ atgql/pyutils/promise_for_object.py
@@ -0,0 +1,14 @@
+import asyncio
+from typing import TypeVar
+
+from atgql.pyutils.obj_map import ObjMap
+from atgql.shims import Promise
+
+T = TypeVar('T')
+
+
+def promise_for_object(obj: ObjMap[Promise[T]]) -> Promise[ObjMap[T]]:
+    async def transformer() -> ObjMap[T]:
+        return dict(zip(obj.keys(), await asyncio.gather(*obj.values())))
+
+    return transformer()
