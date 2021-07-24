2bd074400a311a2adf421fa93d97538a468eace5
iyanging <iyanging@163.com>
2021-07-25 01:14:41 +0800

    feat(pyutils): transform toObjMap.ts -> to_obj_map.py

---
+++ atgql/pyutils/to_obj_map.py
@@ -0,0 +1,19 @@
+from typing import TypeVar, Union, overload
+
+from atgql.pyutils.obj_map import ObjMap, ObjMapLike, ReadOnlyObjMap, ReadOnlyObjMapLike
+
+T = TypeVar('T')
+
+
+@overload
+def to_obj_map(obj: ObjMapLike[T]) -> ObjMap[T]:
+    ...
+
+
+@overload
+def to_obj_map(obj: ReadOnlyObjMapLike[T]) -> ReadOnlyObjMap[T]:
+    ...
+
+
+def to_obj_map(obj: Union[ObjMapLike[T], ReadOnlyObjMapLike[T]]):
+    return obj
