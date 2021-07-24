0151f5bcb393cb29d0d80d21a43fb97b2e2d4606
iyanging <iyanging@163.com>
2021-07-24 23:25:02 +0800

    refactor(pyutils.is_promise): correct the type annotations of the argument and the return

--- atgql/pyutils/is_promise.py
+++ atgql/pyutils/is_promise.py
@@ -1,12 +1,15 @@
 import inspect
-from typing import Any
+from typing import TypeVar
 
 from typing_extensions import TypeGuard
 
+from atgql.pyutils.promise_or_value import PromiseOrValue
 from atgql.shims import Promise
 
+T = TypeVar('T')
 
-def is_promise(value: Any) -> TypeGuard[Promise]:
+
+def is_promise(value: PromiseOrValue[T]) -> TypeGuard[Promise[T]]:
     """Return True if the object can be used in await expression.
 
     Generator-based coroutines are awaitables,
