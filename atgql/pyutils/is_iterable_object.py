d2da28d77ae9f587c2d0ca86c3c84b767a95881d
iyanging <iyanging@163.com>
2021-07-28 23:03:56 +0800

    fix(pyutils.is_iterable_object): is_iterable_object() now treats str as iterable

--- atgql/pyutils/is_iterable_object.py
+++ atgql/pyutils/is_iterable_object.py
@@ -7,9 +7,6 @@ from typing_extensions import TypeGuard
 
 
 def is_iterable_object(maybe_iterable: Any) -> TypeGuard[Iterable[Any]]:
-    """
-    Returns true if the provided object is an Object (i.e. not a string literal)
-    and implements the Iterator protocol.
-    """
+    """Returns true if the provided object implements the Iterator protocol."""
 
-    return not isinstance(maybe_iterable, str) and isinstance(maybe_iterable, Iterable)
+    return isinstance(maybe_iterable, Iterable)
