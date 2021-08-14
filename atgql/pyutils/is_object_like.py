6dff37452c06977d5e85b042fa7298db4f0a47ba
iyanging <iyanging@163.com>
2021-08-15 02:49:08 +0800

    refactor(pyutils.is_object_like): reimplement is_object_like() by using typeof()

--- atgql/pyutils/is_object_like.py
+++ atgql/pyutils/is_object_like.py
@@ -1,9 +1,17 @@
 __all__ = ['is_object_like']
 
+
 from typing import Any
 
 from typing_extensions import TypeGuard
 
+from atgql.shims import typeof
+
 
 def is_object_like(value: Any) -> TypeGuard[object]:
-    return isinstance(value, object)  # will always return True
+    """
+    Return true if `value` is object-like. A value is object-like if it's not
+    `None` and has a `typeof` result of "object".
+    """
+
+    return typeof(value) == 'object' and value is not None
