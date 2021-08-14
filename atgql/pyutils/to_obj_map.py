bb0dca3075abef00a56e904add48333392a35325
iyanging <iyanging@163.com>
2021-08-15 02:51:57 +0800

    refactor(pyutils.to_obj_map): reimplement to_obj_map()

--- atgql/pyutils/to_obj_map.py
+++ atgql/pyutils/to_obj_map.py
@@ -1,6 +1,7 @@
 __all__ = ['to_obj_map']
 
-from typing import TypeVar, Union, overload
+from collections.abc import Mapping
+from typing import TypeVar, overload
 
 from atgql.pyutils.obj_map import ObjMap, ObjMapLike, ReadOnlyObjMap, ReadOnlyObjMapLike
 
@@ -8,7 +9,12 @@ T = TypeVar('T')
 
 
 @overload
-def to_obj_map(obj: ObjMapLike[T]) -> ObjMap[T]:
+def to_obj_map(obj: ObjMapLike[T]) -> ObjMap[T]:  # type: ignore[misc]
+    # Here mypy complains `for:
+    # "Overloaded function signatures 1 and 2 overlap with incompatible return types".
+    # That's wrong, because:
+    # ObjMapLike is a subclass of ReadOnlyObjMapLike, and
+    # ObjMap is a subclass of ReadOnlyObjMap
     ...
 
 
@@ -17,5 +23,28 @@ def to_obj_map(obj: ReadOnlyObjMapLike[T]) -> ReadOnlyObjMap[T]:
     ...
 
 
-def to_obj_map(obj: Union[ObjMapLike[T], ReadOnlyObjMapLike[T]]):
-    return obj
+def to_obj_map(obj):
+    """Convert an object to dict by iterating its attributes.
+
+    Internally it will call vars() if the argument is not a dict,
+    vars() will return the object's __dict__.
+
+    In the implementation of graphql-js, toObjMap() uses Object.entries(),
+    which returns an array of a given object's own enumerable string-keyed property.
+
+    Enumerable properties are usually created via simple assignment or via a property initializer,
+    as for Python, these operations are reflected by __dict__.
+
+    References:
+
+    https://docs.python.org/3/library/functions.html?#vars
+
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Enumerability_and_ownership_of_properties
+    """
+
+    if isinstance(obj, Mapping):
+        return obj
+    else:
+        return dict(vars(obj).items())  # shallow copy, also typecast
