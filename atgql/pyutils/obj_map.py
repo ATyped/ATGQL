23b226a3dce629513a6d64c109441a077d0dd603
iyanging <iyanging@163.com>
2021-08-15 02:50:48 +0800

    fix(pyutils.obj_map): correct the declaration of ReadOnlyObjMapLike; add some comments

--- atgql/pyutils/obj_map.py
+++ atgql/pyutils/obj_map.py
@@ -1,12 +1,20 @@
 __all__ = ['ObjMap', 'ObjMapLike', 'ReadOnlyObjMap', 'ReadOnlyObjMapLike']
 
 from collections.abc import Mapping, MutableMapping
-from typing import TypeVar
+from typing import TypeVar, Union
 
 T = TypeVar('T')
 
+
 ObjMap = MutableMapping[str, T]
+# We cannot assume an object is writable, ...
 ObjMapLike = ObjMap[T]
 
 ReadOnlyObjMap = Mapping[str, T]
-ReadOnlyObjMapLike = ReadOnlyObjMap[T]
+# ..., but we can assume an object is readable,
+# otherwise using this library will be nonsense.
+ReadOnlyObjMapLike = Union[ReadOnlyObjMap[T], object]
+
+
+# Although issubclass cannot be used on parameterized generic,
+# ObjMap is a subclass of ReadOnlyObjMap, and ObjMapLike is a subclass of ReadOnlyObjMapLike
