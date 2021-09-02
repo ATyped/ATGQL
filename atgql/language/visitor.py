0fbeca7dbf5bff4ae7f0f7bc02a6e2f440502238
iyanging <iyanging@163.com>
2021-09-03 00:53:55 +0800

    a little...

--- atgql/language/visitor.py
+++ atgql/language/visitor.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from dataclasses import dataclass
 
 from atgql.language.ast import (
     ASTNode,
@@ -46,7 +47,7 @@ from atgql.language.ast import (
     EnumTypeExtensionNode,
     InputObjectTypeExtensionNode,
 )
-from typing import Any, Callable, Optional, Protocol, TypeVar, Union
+from typing import Any, Callable, Final, Optional, Protocol, TypeVar, Union
 from collections.abc import Sequence
 
 # ASTVisitor should be defined here according to graphql-js,
@@ -362,257 +363,420 @@ ASTNodeType = TypeVar('ASTNodeType', bound=ASTNode, covariant=True)
 R = TypeVar('R')
 
 
-class _NodeReducerWithEnter(Protocol[ASTNodeType, R]):
-    enter: ASTVisitFn[ASTNodeType]
-    leave: ASTReducerFn[ASTNodeType, R]
+# class _NodeReducerWithEnter(Protocol[ASTNodeType, R]):
+#     enter: ASTVisitFn[ASTNodeType]
+#     leave: ASTReducerFn[ASTNodeType, R]
 
 
-class _NodeReducerWithoutEnter(Protocol[ASTNodeType, R]):
-    leave: ASTReducerFn[ASTNodeType, R]
+# class _NodeReducerWithoutEnter(Protocol[ASTNodeType, R]):
+#     leave: ASTReducerFn[ASTNodeType, R]
 
 
-_NodeReducer = Union[
-    _NodeReducerWithEnter[ASTNodeType, R], _NodeReducerWithoutEnter[ASTNodeType, R]
-]
+# _NodeReducer = Union[
+#     _NodeReducerWithEnter[ASTNodeType, R], _NodeReducerWithoutEnter[ASTNodeType, R]
+# ]
 
 
-class _NameReducer(Protocol[R]):
-    name: _NodeReducer[NameNode, R]
+# class _NameReducer(Protocol[R]):
+#     name: _NodeReducer[NameNode, R]
 
 
-class _DocumentReducer(Protocol[R]):
-    document: _NodeReducer[DocumentNode, R]
+# class _DocumentReducer(Protocol[R]):
+#     document: _NodeReducer[DocumentNode, R]
 
 
-class _OperationDefinitionReducer(Protocol[R]):
-    operation_definition: _NodeReducer[OperationDefinitionNode, R]
+# class _OperationDefinitionReducer(Protocol[R]):
+#     operation_definition: _NodeReducer[OperationDefinitionNode, R]
 
 
-class _VariableDefinitionReducer(Protocol[R]):
-    variable_definition: _NodeReducer[VariableDefinitionNode, R]
+# class _VariableDefinitionReducer(Protocol[R]):
+#     variable_definition: _NodeReducer[VariableDefinitionNode, R]
 
 
-class _VariableReducer(Protocol[R]):
-    variable: _NodeReducer[VariableNode, R]
+# class _VariableReducer(Protocol[R]):
+#     variable: _NodeReducer[VariableNode, R]
 
 
-class _SelectionSetReducer(Protocol[R]):
-    selection_set: _NodeReducer[SelectionSetNode, R]
+# class _SelectionSetReducer(Protocol[R]):
+#     selection_set: _NodeReducer[SelectionSetNode, R]
 
 
-class _FieldReducer(Protocol[R]):
-    field: _NodeReducer[FieldNode, R]
+# class _FieldReducer(Protocol[R]):
+#     field: _NodeReducer[FieldNode, R]
 
 
-class _ArgumentReducer(Protocol[R]):
-    argument: _NodeReducer[ArgumentNode, R]
+# class _ArgumentReducer(Protocol[R]):
+#     argument: _NodeReducer[ArgumentNode, R]
 
 
-class _FragmentSpreadReducer(Protocol[R]):
-    fragment_spread: _NodeReducer[FragmentSpreadNode, R]
+# class _FragmentSpreadReducer(Protocol[R]):
+#     fragment_spread: _NodeReducer[FragmentSpreadNode, R]
 
 
-class _InlineFragmentReducer(Protocol[R]):
-    inline_fragment: _NodeReducer[InlineFragmentNode, R]
+# class _InlineFragmentReducer(Protocol[R]):
+#     inline_fragment: _NodeReducer[InlineFragmentNode, R]
 
 
-class _FragmentDefinitionReducer(Protocol[R]):
-    fragment_definition: _NodeReducer[FragmentDefinitionNode, R]
+# class _FragmentDefinitionReducer(Protocol[R]):
+#     fragment_definition: _NodeReducer[FragmentDefinitionNode, R]
 
 
-class _IntValueReducer(Protocol[R]):
-    int_value: _NodeReducer[IntValueNode, R]
+# class _IntValueReducer(Protocol[R]):
+#     int_value: _NodeReducer[IntValueNode, R]
 
 
-class _FloatValueReducer(Protocol[R]):
-    float_value: _NodeReducer[FloatValueNode, R]
+# class _FloatValueReducer(Protocol[R]):
+#     float_value: _NodeReducer[FloatValueNode, R]
 
 
-class _StringValueReducer(Protocol[R]):
-    string_value: _NodeReducer[StringValueNode, R]
+# class _StringValueReducer(Protocol[R]):
+#     string_value: _NodeReducer[StringValueNode, R]
 
 
-class _BooleanValueReducer(Protocol[R]):
-    boolean_value: _NodeReducer[BooleanValueNode, R]
+# class _BooleanValueReducer(Protocol[R]):
+#     boolean_value: _NodeReducer[BooleanValueNode, R]
 
 
-class _NullValueReducer(Protocol[R]):
-    null_value: _NodeReducer[NullValueNode, R]
+# class _NullValueReducer(Protocol[R]):
+#     null_value: _NodeReducer[NullValueNode, R]
 
 
-class _EnumValueReducer(Protocol[R]):
-    enum_value: _NodeReducer[EnumValueNode, R]
+# class _EnumValueReducer(Protocol[R]):
+#     enum_value: _NodeReducer[EnumValueNode, R]
 
 
-class _ListValueReducer(Protocol[R]):
-    list_value: _NodeReducer[ListValueNode, R]
+# class _ListValueReducer(Protocol[R]):
+#     list_value: _NodeReducer[ListValueNode, R]
 
 
-class _ObjectValueReducer(Protocol[R]):
-    object_value: _NodeReducer[ObjectValueNode, R]
+# class _ObjectValueReducer(Protocol[R]):
+#     object_value: _NodeReducer[ObjectValueNode, R]
 
 
-class _ObjectFieldReducer(Protocol[R]):
-    object_field: _NodeReducer[ObjectFieldNode, R]
+# class _ObjectFieldReducer(Protocol[R]):
+#     object_field: _NodeReducer[ObjectFieldNode, R]
 
 
-class _DirectiveReducer(Protocol[R]):
-    directive: _NodeReducer[DirectiveNode, R]
+# class _DirectiveReducer(Protocol[R]):
+#     directive: _NodeReducer[DirectiveNode, R]
 
 
-class _NamedTypeReducer(Protocol[R]):
-    named_type: _NodeReducer[NamedTypeNode, R]
+# class _NamedTypeReducer(Protocol[R]):
+#     named_type: _NodeReducer[NamedTypeNode, R]
 
 
-class _ListTypeReducer(Protocol[R]):
-    list_type: _NodeReducer[ListTypeNode, R]
+# class _ListTypeReducer(Protocol[R]):
+#     list_type: _NodeReducer[ListTypeNode, R]
 
 
-class _NonNullTypeReducer(Protocol[R]):
-    non_null_type: _NodeReducer[NonNullTypeNode, R]
+# class _NonNullTypeReducer(Protocol[R]):
+#     non_null_type: _NodeReducer[NonNullTypeNode, R]
 
 
-class _SchemaDefinitionReducer(Protocol[R]):
-    schema_definition: _NodeReducer[SchemaDefinitionNode, R]
+# class _SchemaDefinitionReducer(Protocol[R]):
+#     schema_definition: _NodeReducer[SchemaDefinitionNode, R]
 
 
-class _OperationTypeDefinitionReducer(Protocol[R]):
-    operation_type_definition: _NodeReducer[OperationTypeDefinitionNode, R]
+# class _OperationTypeDefinitionReducer(Protocol[R]):
+#     operation_type_definition: _NodeReducer[OperationTypeDefinitionNode, R]
 
 
-class _ScalarTypeDefinitionReducer(Protocol[R]):
-    scalar_type_definition: _NodeReducer[ScalarTypeDefinitionNode, R]
+# class _ScalarTypeDefinitionReducer(Protocol[R]):
+#     scalar_type_definition: _NodeReducer[ScalarTypeDefinitionNode, R]
 
 
-class _ObjectTypeDefinitionReducer(Protocol[R]):
-    object_type_definition: _NodeReducer[ObjectTypeDefinitionNode, R]
+# class _ObjectTypeDefinitionReducer(Protocol[R]):
+#     object_type_definition: _NodeReducer[ObjectTypeDefinitionNode, R]
 
 
-class _FieldDefinitionReducer(Protocol[R]):
-    field_definition: _NodeReducer[FieldDefinitionNode, R]
+# class _FieldDefinitionReducer(Protocol[R]):
+#     field_definition: _NodeReducer[FieldDefinitionNode, R]
 
 
-class _InputValueDefinitionReducer(Protocol[R]):
-    input_value_definition: _NodeReducer[InputValueDefinitionNode, R]
+# class _InputValueDefinitionReducer(Protocol[R]):
+#     input_value_definition: _NodeReducer[InputValueDefinitionNode, R]
 
 
-class _InterfaceTypeDefinitionReducer(Protocol[R]):
-    interface_type_definition: _NodeReducer[InterfaceTypeDefinitionNode, R]
+# class _InterfaceTypeDefinitionReducer(Protocol[R]):
+#     interface_type_definition: _NodeReducer[InterfaceTypeDefinitionNode, R]
 
 
-class _UnionTypeDefinitionReducer(Protocol[R]):
-    union_type_definition: _NodeReducer[UnionTypeDefinitionNode, R]
+# class _UnionTypeDefinitionReducer(Protocol[R]):
+#     union_type_definition: _NodeReducer[UnionTypeDefinitionNode, R]
 
 
-class _EnumTypeDefinitionReducer(Protocol[R]):
-    enum_type_definition: _NodeReducer[EnumTypeDefinitionNode, R]
+# class _EnumTypeDefinitionReducer(Protocol[R]):
+#     enum_type_definition: _NodeReducer[EnumTypeDefinitionNode, R]
 
 
-class _EnumValueDefinitionReducer(Protocol[R]):
-    enum_value_definition: _NodeReducer[EnumValueDefinitionNode, R]
+# class _EnumValueDefinitionReducer(Protocol[R]):
+#     enum_value_definition: _NodeReducer[EnumValueDefinitionNode, R]
 
 
-class _InputObjectTypeDefinitionReducer(Protocol[R]):
-    input_object_type_definition: _NodeReducer[InputObjectTypeDefinitionNode, R]
+# class _InputObjectTypeDefinitionReducer(Protocol[R]):
+#     input_object_type_definition: _NodeReducer[InputObjectTypeDefinitionNode, R]
 
 
-class _DirectiveDefinitionReducer(Protocol[R]):
-    directive_definition: _NodeReducer[DirectiveDefinitionNode, R]
+# class _DirectiveDefinitionReducer(Protocol[R]):
+#     directive_definition: _NodeReducer[DirectiveDefinitionNode, R]
 
 
-class _SchemaExtensionReducer(Protocol[R]):
-    schema_extension: _NodeReducer[SchemaExtensionNode, R]
+# class _SchemaExtensionReducer(Protocol[R]):
+#     schema_extension: _NodeReducer[SchemaExtensionNode, R]
 
 
-class _ScalarTypeExtensionReducer(Protocol[R]):
-    scalar_type_extension: _NodeReducer[ScalarTypeExtensionNode, R]
+# class _ScalarTypeExtensionReducer(Protocol[R]):
+#     scalar_type_extension: _NodeReducer[ScalarTypeExtensionNode, R]
 
 
-class _ObjectTypeExtensionReducer(Protocol[R]):
-    object_type_extension: _NodeReducer[ObjectTypeExtensionNode, R]
+# class _ObjectTypeExtensionReducer(Protocol[R]):
+#     object_type_extension: _NodeReducer[ObjectTypeExtensionNode, R]
 
 
-class _InterfaceTypeExtensionReducer(Protocol[R]):
-    interface_type_extension: _NodeReducer[InterfaceTypeExtensionNode, R]
+# class _InterfaceTypeExtensionReducer(Protocol[R]):
+#     interface_type_extension: _NodeReducer[InterfaceTypeExtensionNode, R]
 
 
-class _UnionTypeExtensionReducer(Protocol[R]):
-    union_type_extension: _NodeReducer[UnionTypeExtensionNode, R]
+# class _UnionTypeExtensionReducer(Protocol[R]):
+#     union_type_extension: _NodeReducer[UnionTypeExtensionNode, R]
 
 
-class _EnumTypeExtensionReducer(Protocol[R]):
-    enum_type_extension: _NodeReducer[EnumTypeExtensionNode, R]
+# class _EnumTypeExtensionReducer(Protocol[R]):
+#     enum_type_extension: _NodeReducer[EnumTypeExtensionNode, R]
 
 
-class _InputObjectTypeExtensionReducer(Protocol[R]):
-    input_object_type_extension: _NodeReducer[InputObjectTypeExtensionNode, R]
+# class _InputObjectTypeExtensionReducer(Protocol[R]):
+#     input_object_type_extension: _NodeReducer[InputObjectTypeExtensionNode, R]
 
 
-ASTReducer = Union[
-    _NameReducer,
-    _DocumentReducer,
-    _OperationDefinitionReducer,
-    _VariableDefinitionReducer,
-    _VariableReducer,
-    _SelectionSetReducer,
-    _FieldReducer,
-    _ArgumentReducer,
-    _FragmentSpreadReducer,
-    _InlineFragmentReducer,
-    _FragmentDefinitionReducer,
-    _IntValueReducer,
-    _FloatValueReducer,
-    _StringValueReducer,
-    _BooleanValueReducer,
-    _NullValueReducer,
-    _EnumValueReducer,
-    _ListValueReducer,
-    _ObjectValueReducer,
-    _ObjectFieldReducer,
-    _DirectiveReducer,
-    _NamedTypeReducer,
-    _ListTypeReducer,
-    _NonNullTypeReducer,
-    _SchemaDefinitionReducer,
-    _OperationTypeDefinitionReducer,
-    _ScalarTypeDefinitionReducer,
-    _ObjectTypeDefinitionReducer,
-    _FieldDefinitionReducer,
-    _InputValueDefinitionReducer,
-    _InterfaceTypeDefinitionReducer,
-    _UnionTypeDefinitionReducer,
-    _EnumTypeDefinitionReducer,
-    _EnumValueDefinitionReducer,
-    _InputObjectTypeDefinitionReducer,
-    _DirectiveDefinitionReducer,
-    _SchemaExtensionReducer,
-    _ScalarTypeExtensionReducer,
-    _ObjectTypeExtensionReducer,
-    _InterfaceTypeExtensionReducer,
-    _UnionTypeExtensionReducer,
-    _EnumTypeExtensionReducer,
-    _InputObjectTypeExtensionReducer,
-]
+# ASTReducer = Union[
+#     _NameReducer,
+#     _DocumentReducer,
+#     _OperationDefinitionReducer,
+#     _VariableDefinitionReducer,
+#     _VariableReducer,
+#     _SelectionSetReducer,
+#     _FieldReducer,
+#     _ArgumentReducer,
+#     _FragmentSpreadReducer,
+#     _InlineFragmentReducer,
+#     _FragmentDefinitionReducer,
+#     _IntValueReducer,
+#     _FloatValueReducer,
+#     _StringValueReducer,
+#     _BooleanValueReducer,
+#     _NullValueReducer,
+#     _EnumValueReducer,
+#     _ListValueReducer,
+#     _ObjectValueReducer,
+#     _ObjectFieldReducer,
+#     _DirectiveReducer,
+#     _NamedTypeReducer,
+#     _ListTypeReducer,
+#     _NonNullTypeReducer,
+#     _SchemaDefinitionReducer,
+#     _OperationTypeDefinitionReducer,
+#     _ScalarTypeDefinitionReducer,
+#     _ObjectTypeDefinitionReducer,
+#     _FieldDefinitionReducer,
+#     _InputValueDefinitionReducer,
+#     _InterfaceTypeDefinitionReducer,
+#     _UnionTypeDefinitionReducer,
+#     _EnumTypeDefinitionReducer,
+#     _EnumValueDefinitionReducer,
+#     _InputObjectTypeDefinitionReducer,
+#     _DirectiveDefinitionReducer,
+#     _SchemaExtensionReducer,
+#     _ScalarTypeExtensionReducer,
+#     _ObjectTypeExtensionReducer,
+#     _InterfaceTypeExtensionReducer,
+#     _UnionTypeExtensionReducer,
+#     _EnumTypeExtensionReducer,
+#     _InputObjectTypeExtensionReducer,
+# ]
 
-TReducedNode = TypeVar('TReducedNode', bound=ASTNode, covariant=True)
+# TReducedNode = TypeVar('TReducedNode', bound=ASTNode, covariant=True)
 
 
-ASTReducerFn = Callable[
-    [
-        ASTNodeType,
-        Optional[str | int],
-        Optional[ASTNode | Sequence[ASTNode]],
-        Sequence[str | int],
-        Sequence[ASTNode | Sequence[ASTNode]],
+# ASTReducerFn = Callable[
+#     [
+#         ASTNodeType,
+#         Optional[str | int],
+#         Optional[ASTNode | Sequence[ASTNode]],
+#         Sequence[str | int],
+#         Sequence[ASTNode | Sequence[ASTNode]],
+#     ],
+#     R,
+# ]
+
+
+# T = TypeVar('T')
+
+# ReducedField = Union[Optional[T], Sequence[T], R]
+
+
+query_document_keys: Final = {
+    'Name': [],
+    'Document': ['definitions'],
+    'OperationDefinition': ['name', 'variable_definitions', 'directives', 'selection_set'],
+    'VariableDefinition': ['variable', 'type', 'default_value', 'directives'],
+    'Variable': ['name'],
+    'SelectionSet': ['selections'],
+    'Field': ['alias', 'name', 'arguments', 'directives', 'selection_set'],
+    'Argument': ['name', 'value'],
+    'FragmentSpread': ['name', 'directives'],
+    'InlineFragment': ['type_condition', 'directives', 'selection_set'],
+    'FragmentDefinition': [
+        'name',
+        # Note: fragment variable definitions are deprecated and will removed in v17.0.0
+        'variable_definitions',
+        'type_condition',
+        'directives',
+        'selection_set',
     ],
-    R,
-]
+    'IntValue': [],
+    'FloatValue': [],
+    'StringValue': [],
+    'BooleanValue': [],
+    'NullValue': [],
+    'EnumValue': [],
+    'ListValue': ['values'],
+    'ObjectValue': ['fields'],
+    'ObjectField': ['name', 'value'],
+    'Directive': ['name', 'arguments'],
+    'NamedType': ['name'],
+    'ListType': ['type'],
+    'NonNullType': ['type'],
+    'SchemaDefinition': ['description', 'directives', 'operation_types'],
+    'OperationTypeDefinition': ['type'],
+    'ScalarTypeDefinition': ['description', 'name', 'directives'],
+    'ObjectTypeDefinition': [
+        'description',
+        'name',
+        'interfaces',
+        'directives',
+        'fields',
+    ],
+    'FieldDefinition': ['description', 'name', 'arguments', 'type', 'directives'],
+    'InputValueDefinition': [
+        'description',
+        'name',
+        'type',
+        'default_value',
+        'directives',
+    ],
+    'InterfaceTypeDefinition': [
+        'description',
+        'name',
+        'interfaces',
+        'directives',
+        'fields',
+    ],
+    'UnionTypeDefinition': ['description', 'name', 'directives', 'types'],
+    'EnumTypeDefinition': ['description', 'name', 'directives', 'values'],
+    'EnumValueDefinition': ['description', 'name', 'directives'],
+    'InputObjectTypeDefinition': ['description', 'name', 'directives', 'fields'],
+    'DirectiveDefinition': ['description', 'name', 'arguments', 'locations'],
+    'SchemaExtension': ['directives', 'operation_types'],
+    'ScalarTypeExtension': ['name', 'directives'],
+    'ObjectTypeExtension': ['name', 'interfaces', 'directives', 'fields'],
+    'InterfaceTypeExtension': ['name', 'interfaces', 'directives', 'fields'],
+    'UnionTypeExtension': ['name', 'directives', 'types'],
+    'EnumTypeExtension': ['name', 'directives', 'values'],
+    'InputObjectTypeExtension': ['name', 'directives', 'fields'],
+}
+
+@dataclass
+class _Stack:
+    in_array: bool
+    index: int
+    keys: list[ASTNode]
+    edits: list[tuple[str | int, Any]]
+    prev: Optional[_Stack]
 
 
-T = TypeVar('T')
+def visit(root: ASTNode, visitor: ASTVisitor) -> ASTNode:
+    stack: Optional[_Stack] = None
+    in_array = isinstance(root, Sequence)
 
-# TODO: re-consider implementation
-ReducedField = Union[Optional[T], Sequence[T], R]
+
+#  visit() will walk through an AST using a depth-first traversal, calling
+#  the visitor's enter function at each node in the traversal, and calling the
+#  leave function after visiting that node and all of its child nodes.
+#
+#  By returning different values from the enter and leave functions, the
+#  behavior of the visitor can be altered, including skipping over a sub-tree of
+#  the AST (by returning false), editing the AST by returning a value or null
+#  to remove the value, or to stop the whole traversal by returning BREAK.
+#
+#  When using visit() to edit an AST, the original AST will not be modified, and
+#  a new version of the AST with the changes applied will be returned from the
+#  visit function.
+#
+#  ```ts
+#  const editedAST = visit(ast, {
+#    enter(node, key, parent, path, ancestors) {
+#      // @return
+#      //   undefined: no action
+#      //   false: skip visiting this node
+#      //   visitor.BREAK: stop visiting altogether
+#      //   null: delete this node
+#      //   any value: replace this node with the returned value
+#    },
+#    leave(node, key, parent, path, ancestors) {
+#      // @return
+#      //   undefined: no action
+#      //   false: no action
+#      //   visitor.BREAK: stop visiting altogether
+#      //   null: delete this node
+#      //   any value: replace this node with the returned value
+#    }
+#  });
+#  ```
+#
+#  Alternatively to providing enter() and leave() functions, a visitor can
+#  instead provide functions named the same as the kinds of AST nodes, or
+#  enter/leave visitors at a named key, leading to three permutations of the
+#  visitor API:
+#
+#  1) Named visitors triggered when entering a node of a specific kind.
+#
+#  ```ts
+#  visit(ast, {
+#    Kind(node) {
+#      // enter the "Kind" node
+#    }
+#  })
+#  ```
+#
+#  2) Named visitors that trigger upon entering and leaving a node of a specific kind.
+#
+#  ```ts
+#  visit(ast, {
+#    Kind: {
+#      enter(node) {
+#        // enter the "Kind" node
+#      }
+#      leave(node) {
+#        // leave the "Kind" node
+#      }
+#    }
+#  })
+#  ```
+#
+#  3) Generic visitors that trigger upon entering and leaving any node.
+#
+#  ```ts
+#  visit(ast, {
+#    enter(node) {
+#      // enter any node
+#    },
+#    leave(node) {
+#      // leave any node
+#    }
+#  })
+#  ```
+
 
 # ASTVisitor should not be defined here according to graphql-js,
 # but confined by Python's syntax, ASTNode can only refer the types defined above,
--- atgql/shims.py
+++ atgql/shims.py
@@ -1,9 +1,12 @@
 __all__ = ['Promise', 'typeof']
 
+
 import types
-from collections.abc import Awaitable, Callable
-from inspect import getmembers, isclass
-from typing import Any, Literal, TypeVar
+from abc import ABC
+from collections.abc import Awaitable, Callable, Iterable, MutableSequence
+from inspect import getmembers, isclass, signature
+from typing import Any, Generic, Literal, Optional, TypeVar, Union, cast, overload
+from typing_extensions import TypeGuard
 
 T = TypeVar('T')
 
@@ -25,7 +28,9 @@ for _name, _attr in getmembers(types, isclass):
     if issubclass(_attr, Callable):  # type: ignore[arg-type]
         callable_types.add(_attr)
     elif _attr in (
-        types.CellType,
+        # In Python3.9, mypy reports: Module has no attribute "CellType"  [attr-defined]
+        # But actually it is there.
+        types.CellType,  # type: ignore[attr-defined]
         types.ModuleType,
         types.MappingProxyType,
         types.SimpleNamespace,
@@ -81,3 +86,104 @@ def typeof(value: Any) -> Literal['object', 'boolean', 'number', 'string', 'func
         return 'object'
     else:
         return 'object'
+
+
+U = TypeVar('U')
+S = TypeVar('S')
+
+
+class Array(MutableSequence[T], ABC, Generic[T]):
+    @staticmethod
+    def is_array(value: Any) -> TypeGuard['MutableSequence[T]']:
+        return isinstance(value, MutableSequence) and not isinstance(value, str)
+
+    @overload
+    @staticmethod
+    def from_(
+        iterable: Iterable[T], map_fn: Callable[[S, T], U], self_arg: S, /
+    ) -> MutableSequence[U]:
+        ...
+
+    @overload
+    @staticmethod
+    def from_(
+        iterable: Iterable[T], map_fn: Callable[[S, T, int], U], self_arg: S, /
+    ) -> MutableSequence[U]:
+        ...
+
+    @overload
+    @staticmethod
+    def from_(iterable: Iterable[T], /) -> MutableSequence[T]:
+        ...
+
+    @overload
+    @staticmethod
+    def from_(iterable: Iterable[T], map_fn: Callable[[T], U], /) -> MutableSequence[U]:
+        ...
+
+    @overload
+    @staticmethod
+    def from_(iterable: Iterable[T], map_fn: Callable[[T, int], U], /) -> MutableSequence[U]:
+        ...
+
+    # mypy: Overloaded function implementation does not accept all possible arguments of signature 5
+    # pyright: 0 error, 0 warnings, 0 infos
+    @staticmethod  # type: ignore[misc]
+    def from_(
+        iterable: Iterable[T],
+        map_fn: Optional[
+            Union[
+                Callable[[S, T], U],
+                Callable[[S, T, int], U],
+                Callable[[T], U],
+                Callable[[T, int], U],
+            ]
+        ] = None,
+        self_arg: Optional[S] = None,
+        /,
+    ) -> Any:
+        if self_arg is not None:
+            map_fn = cast(Optional[Union[Callable[[S, T], U], Callable[[S, T, int], U]]], map_fn)
+
+            if map_fn is None:
+                raise ValueError('`map_fn` cannot be None when `self_arg` is not None.')
+
+            # https://docs.python.org/3/howto/descriptor.html#functions-and-methods
+            bound_map_fn = cast(
+                Union[Callable[[T], U], Callable[[T, int], U]],
+                types.MethodType(map_fn, self_arg),
+            )
+
+            # bound method's signature will lost self argument
+            param_count_without_self = len(signature(bound_map_fn).parameters)
+
+            if param_count_without_self == 1:
+                bound_map_fn = cast(Callable[[T], U], bound_map_fn)
+
+                return [bound_map_fn(e) for e in iterable]  # pylint: disable=not-callable
+
+            else:
+                bound_map_fn = cast(Callable[[T, int], U], bound_map_fn)
+
+                return [
+                    bound_map_fn(e, i) for i, e in enumerate(iterable)  # pylint: disable=not-callable
+                ]
+
+        else:
+            map_fn = cast(Optional[Union[Callable[[T], U], Callable[[T, int], U]]], map_fn)
+
+            if map_fn is None:
+                return list(iterable)
+
+            else:
+                param_count = len(signature(map_fn).parameters)
+
+                if param_count == 1:
+                    map_fn = cast(Callable[[T], U], map_fn)
+
+                    return [map_fn(e) for e in iterable]
+
+                else:
+                    map_fn = cast(Callable[[T, int], U], map_fn)
+
+                    return [map_fn(e, i) for i, e in enumerate(iterable)]
